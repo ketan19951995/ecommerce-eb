3f69f63b70e35bc3b12049ce29bd87c1ab07ae5b
unknown <tayalket@gmail.com>
2021-09-19 15:16:49 +0530

    code optimize and validation add

--- app/controller/product.js
+++ app/controller/product.js
@@ -1,12 +1,16 @@
 const { Product, UserProduct } = require('../models/product');
 const mongoose = require('mongoose');
 const helper = require('../common/helper')
+const jwt = require('jsonwebtoken');
 exports.createProduct = async (req, res) => {
     let { name, category, brand } = req.body;
     let newProduct = {
         name,
         category,
         brand
+    }
+    if (!name || !category || !brand) {
+        res.send(helper.respondWithResult(422, { message: "Missing name  , category or brand", result: {} }));
     }
     let productRecord = new Product(newProduct)
     try {
@@ -38,14 +42,22 @@ exports.deleteProduct = async (req, res) => {
 
 exports.updateProduct = async (req, res) => {
     let { id: productId } = req.params;
-    let { name } = req.body;
+    let { name, category, brand } = req.body;
     try {
-        let result = await Product.findOneAndUpdate({ _id: productId }, { name });
-        res.send(helper.respondWithResult(200, { message: "product updated successfully", result }));
+        if (mongoose.Types.ObjectId.isValid(productId)) {
+            let productRecord = await Product.findById(productId).exec();
+            if (productRecord) {
+                let result = await Product.findOneAndUpdate({ _id: productId }, { name, brand, category });
+                res.send(helper.respondWithResult(200, { message: "product updated successfully", result }));
+            }
+        } else {
+            res.send(helper.respondWithResult(404, { message: `No product found with the given Id ${productId}`, "result": "" }));
+        }
     } catch (err) {
         res.send(helper.handleError(err));
     }
-};
+}
+
 
 exports.getAllProducts = async (req, res) => {
     try {
@@ -60,6 +72,9 @@ exports.getAllProducts = async (req, res) => {
 exports.assignProductToUser = async (req, res) => {
     try {
         let { userId, productId } = req.body
+        if (!userId || !productId) {
+            res.send(helper.respondWithResult(422, { message: "Missing  userId or productId", result: {} }));
+        }
         let newProduct = {
             userId,
             productId,
@@ -77,17 +92,22 @@ exports.assignProductToUser = async (req, res) => {
 
 exports.getUserProducts = async (req, res) => {
     try {
-        let { userId , brand , category} = req.query;
+        let { brand, category } = req.query;
+        let { authorization: token } = req.headers
+
+        // get userId from token
+        let decoded = jwt.verify(token, process.env.SECRETKEY);
+        let { ID: userId } = decoded;
+
+        if (!brand || !category) {
+            res.send(helper.respondWithResult(422, { message: "Missing  userId or brand or category", result: {} }));
+        }
         let productRecord = await UserProduct.find({ "userId": userId }).exec();
-        console.log("productRecord" , productRecord);
 
         // filter by category and brand name
         const productIds = productRecord.map(v => v.productId)
-
-        let result = await Product.find( { _id: { $in: productIds }, brand , category } ).exec();
-        console.log("result" , result);
-
-        res.send(helper.respondWithResult(200, { message: "get all the user products", "result" : result }));
+        let result = await Product.find({ _id: { $in: productIds }, brand, category }).exec();
+        res.send(helper.respondWithResult(200, { message: "Filtered prodcuts by brand and category", "result": result }));
     } catch (err) {
         res.send(helper.handleError(err));
     }
--- app/controller/user.js
+++ app/controller/user.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 exports.signup = async (req, res) => {
     let { name, email, password } = req.body;
     if (!email || !password) {
-        res.send(helper.respondWithResult(422, { message: "Missing email or password", result }));
+        res.send(helper.respondWithResult(422, { message: "Missing email or password", result : {} }));
     }
     let hashedPassword = await helper.hashPassword(password);
     try {
@@ -34,6 +34,9 @@ exports.signup = async (req, res) => {
 
 exports.login = async (req, res) => {
     let { email, password } = req.body;
+    if (!email || !password) {
+        res.send(helper.respondWithResult(422, { message: "Missing email or password", result : {} }));
+    }
     try {
         let result = await User.findOne({ "email": email });
         if (!result) {
@@ -43,7 +46,7 @@ exports.login = async (req, res) => {
         let result1 = await helper.comparePassword(password, hashedPassword);
         const verificationToken = await helper.generateVerificationToken(result._id);
         if (result1) {
-            res.send(helper.respondWithResult(200, { message: "Login Successfull", "result" : verificationToken }));
+            res.send(helper.respondWithResult(200, { message: "Login Successfull", "result": verificationToken }));
         } else {
             res.send(helper.respondWithResult(200, { message: "Password is wrong", result: "" }));
         }
@@ -59,7 +62,7 @@ exports.verifyToken = async (req, res) => {
         let { ID } = decoded;
         let result = await User.findByIdAndUpdate({ "_id": mongoose.Types.ObjectId(ID) }, { "isUsed": true });
         if (result) {
-            res.send(helper.respondWithResult(200, { message: "user verified successfully", result }));
+            res.send(helper.respondWithResult(200, { message: "user verified successfully", result: {} }));
         } else {
             res.send(helper.respondWithResult(404, { message: "Id not found", result }));
         }
